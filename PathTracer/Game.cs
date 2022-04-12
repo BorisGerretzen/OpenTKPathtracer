38e24f2cfa001eafbcc9e4360f784ba4d6f1261a
Boris Gerretzen <borisgerretzen@gmail.com>
2022-04-12 23:05:16 +0200

    Initial stuff

---
+++ PathTracer/Game.cs
@@ -0,0 +1,23 @@
+﻿using OpenTK.Graphics.OpenGL;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace PathTracer; 
+
+public class Game : GameWindow{
+    public Game(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings) { }
+
+    protected override void OnUpdateFrame(FrameEventArgs args) {
+        if (KeyboardState.IsKeyDown(Keys.Escape)) {
+            Close();
+        }
+        base.OnUpdateFrame(args);
+    }
+
+    protected override void OnRenderFrame(FrameEventArgs args) {
+        GL.ClearColor(1, 0, 0, 1);
+        GL.Clear(ClearBufferMask.ColorBufferBit);
+        SwapBuffers();
+    }
+}
---
+++ PathTracer/Program.cs
@@ -0,0 +1,19 @@
+﻿// This line creates a new instance, and wraps the instance in a using statement so it's automatically disposed once we've exited the block.
+
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Desktop;
+using PathTracer;
+
+var gameWindowSettings = new GameWindowSettings();
+gameWindowSettings.RenderFrequency = 144;
+gameWindowSettings.UpdateFrequency = 144;
+
+var nativeWindowSettings = new NativeWindowSettings();
+nativeWindowSettings.Size = new Vector2i(800, 600);
+nativeWindowSettings.Title = "Pathtracer?";
+
+using (Game game = new Game(gameWindowSettings, nativeWindowSettings)) {
+    //Run takes a double, which is how many frames per second it should strive to reach.
+    //You can leave that out and it'll just update as fast as the hardware will allow it.
+    game.Run();
+}
